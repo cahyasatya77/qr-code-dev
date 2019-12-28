8728012be35c5c0b144c123356ad12df36fcb1ca
fujitora <cahyasatya77@gmail.com>
2019-12-28 12:07:22 +0700

    penambahan koneksi TCP socket di nodejs file server.js

--- nodejs/server.js
+++ nodejs/server.js
@@ -32,6 +32,31 @@ io.on('connection', function(socket){
 //    io.emit('data', {data: data});
 //})
 
+// Connection TCP received data
+//var net = require('net');
+//
+//var HOST = '192.168.250.2';
+//var PORT = 2001;
+//
+//var client = new net.Socket();
+//client.connect(PORT, HOST, function () {
+//   console.log('CONNECTED : ' + HOST + ' : ' + PORT);
+//   // Write a message to the socket as soon as the client is connected, the server will receive it as message from the client
+//   client.write('I am Cahya Satya');
+//});
+//// Add a 'data' event handler for the client socket
+//// data is what the server sent to this socket
+//client.on('data', function (data) {
+//    console.log('Data : ' + data);
+//    // Close the client socket completely
+//    client.destroy();
+//});
+//
+//// Add a 'close' event handler for the client socket
+//client.on('close', function() {
+//  console.log('Connection closed');
+//});
+
 // server start
 http.listen(port, function(){
 	console.log("Node server listening on port " + port);
